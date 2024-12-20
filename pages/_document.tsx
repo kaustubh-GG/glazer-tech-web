14dc8e85d80cdde2bbda72186bbbcbe3068c3fa0
kaustubh-GG <sulekaustubh97@gmail.com>
2024-12-20 14:24:18 +0530

    added fav icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,13 +16,13 @@ export default class Document extends NextDocument {
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
+            href="/static/favicons/favicon2nnn.ico"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
+            href="/static/favicons/favicon2nnn.ico"
           />
           <link rel="manifest" href="/static/favicons/manifest.json" />
         </Head>
